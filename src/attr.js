f9a61c8f9d4c8daf837fc4c82fa6f562d8a3e6c0
Vivek Kumar Verma <v2verma@users.noreply.github.com>
2018-06-07 17:34:32 +0530

    Create attr.js
    The code is to get attributes of any DOM element and display the same using jQuery.

---
+++ src/attr.js
@@ -0,0 +1,31 @@
+import React,{Component} from 'react';
+import { render } from 'react-dom';
+import $ from 'jquery';
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+  componentDidMount(){
+    var attr1 = $("#link").attr('target');
+    var attr2 = $("#link").attr('href');
+    $('#attr').text(attr2);
+    // $('#attr').text(attr1);
+  }
+
+  render(){
+    return(
+      <div >
+        <h2>jQuery Examples : html</h2>
+        <div>
+          <p>
+            <a id="link" href="www.google.com" target="_blank">Google Link</a>
+          </p>
+          <div id="attr"></div>
+        </div>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('root'));
